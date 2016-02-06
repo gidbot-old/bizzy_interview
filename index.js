a87e39dcee57e10828c55c14770b8f6132dfe97d
gideon <gideon@blend.la>
2016-02-05 17:57:18 -0800

    adding try-catches

--- index.js
+++ index.js
@@ -10,32 +10,36 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-	req.session.url =  'https://' + req.body.name + '.myshopify.com';
-
-	var post_data = { 
-		'utf8':'✓',
-		'redirect': '',
-		'subdomain':req.body.name,
-		'login':req.body.email,
-		'password':req.body.pwd
-	}
-
-	var headers = { 
-		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
-    	'Content-Type' : 'application/x-www-form-urlencoded' 
-	};
-	var url = req.session.url + '/admin/auth/login';
-	request.post({ url: url, form: post_data, headers: headers }, function (err, response, body) {
-		var shopCookies = response.headers['set-cookie'];
-
-		var j = request.jar();
-		for (var i = 0; i < shopCookies.length; i++) {
-			var cookie = request.cookie(shopCookies[i]);
-			j.setCookie(cookie, url);
+	try {
+		req.session.url =  'https://' + req.body.name + '.myshopify.com';
+		var post_data = { 
+			'utf8':'✓',
+			'redirect': '',
+			'subdomain':req.body.name,
+			'login':req.body.email,
+			'password':req.body.pwd
 		}
-		req.session.cookie_string = j.getCookieString(url);
-		res.redirect('/discounts/new');
-	});
+
+		var headers = { 
+			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
+	    	'Content-Type' : 'application/x-www-form-urlencoded' 
+		};
+		var url = req.session.url + '/admin/auth/login';
+		request.post({ url: url, form: post_data, headers: headers }, function (err, response, body) {
+			var shopCookies = response.headers['set-cookie'];
+
+			var j = request.jar();
+			for (var i = 0; i < shopCookies.length; i++) {
+				var cookie = request.cookie(shopCookies[i]);
+				j.setCookie(cookie, url);
+			}
+			req.session.cookie_string = j.getCookieString(url);
+			res.redirect('/discounts/new');
+		});
+	} catch (e) {
+		res.redirect('/');
+	}
+	
 });
 
 router.get('/discounts/new', function (req, res) {
@@ -51,50 +55,53 @@ router.post('/discounts/new', function (req, res) {
 	if (!req.session.cookie_string) {
 		res.redirect('/');
 	} else {
+		try {
+			var url = req.session.url + '/admin/discounts/new'; 
+			var headers = { 
+				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
+	    		'Content-Type' : 'application/x-www-form-urlencoded',
+	    		'Cookie': req.session.cookie_string
+			};
 
-		var url = req.session.url + '/admin/discounts/new'; 
-		var headers = { 
-			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
-    		'Content-Type' : 'application/x-www-form-urlencoded',
-    		'Cookie': req.session.cookie_string
-		};
+			request.get({ url: url, headers: headers}, function (err, response, body) {
+				var value = response.body.match(/name="authenticity_token" value=".*"/i)[0];
+				var index = value.indexOf('value="');
+				value = (value.substring(index+7, value.length-1));
 
-		request.get({ url: url, headers: headers}, function (err, response, body) {
-			var value = response.body.match(/name="authenticity_token" value=".*"/i)[0];
-			var index = value.indexOf('value="');
-			value = (value.substring(index+7, value.length-1));
+				var count = parseInt(req.body.amount) + 1;
+				var checkCount = count;  
+				var codes = [];
+				for (var i =1; i < count; i++) {
+					var post_data = { 
+					  utf8: '✓',
+					  authenticity_token: value,
+					  discount: 
+					   { code: req.body.code+"_"+i,
+					     discount_type: req.body.discount_type,
+					     value: parseInt(req.body.value),
+					     applies_to_resource: '',
+					     starts_at: '2016-02-09' },
+					  	'unlimited-uses': '',
+					 	 discount_never_expires: '' 
+					}
 
-			var count = parseInt(req.body.amount) + 1;
-			var checkCount = count;  
-			var codes = [];
-			for (var i =1; i < count; i++) {
-				var post_data = { 
-				  utf8: '✓',
-				  authenticity_token: value,
-				  discount: 
-				   { code: req.body.code+"_"+i,
-				     discount_type: req.body.discount_type,
-				     value: parseInt(req.body.value),
-				     applies_to_resource: '',
-				     starts_at: '2016-02-09' },
-				  	'unlimited-uses': '',
-				 	 discount_never_expires: '' 
+					codes.push(post_data.discount);
+
+					var url =  req.session.url+ '/admin/discounts'; 
+					request.post({ url: url, form: post_data, headers: headers }, function (err, response, body) {
+						checkCount--; 
+						if (checkCount < 2) {
+							res.send(codes);
+						}
+					});
+
 				}
 
-				codes.push(post_data.discount);
 
-				var url =  req.session.url+ '/admin/discounts'; 
-				request.post({ url: url, form: post_data, headers: headers }, function (err, response, body) {
-					checkCount--; 
-					if (checkCount < 2) {
-						res.send(codes);
-					}
-				});
-
-			}
-
-
-		});
+			});
+		} catch (e) {
+			res.redirect('/');
+		}
 	}
 });
 
